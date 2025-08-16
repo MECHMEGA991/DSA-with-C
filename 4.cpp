5cdc45eba7692f6e951122a2b355c1e9b8e36f3e
MECHMEGA991 <ankushbhandari664@gmail.com>
2025-08-16 16:21:11 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+// Binary search 
+
+using namespace std;
+
+int main(){
+int size , i , n;
+
+cout<<"Enter the size =";
+cin>>size;
+
+int arr[size],store[size];
+
+int flag =0;
+
+cout<<"Enter the elements of array :"<<endl;
+
+for(i=0;i<size;i++){
+    cin>>arr[i];
+}
+cout<<"Enter the Elements U want to search ";
+cin>>n;
+
+for(i=0;i<size;i++){
+    if(arr[i]==n){
+        store[flag]=i;
+        flag++;
+        
+    }
+    
+}
+    if(flag == 0){
+        cout<<"Elemnt not found";
+    }
+
+    else{
+    cout<<"Element found at : "<<endl;
+    for(i=0;i<flag;i++){
+
+        cout<<store[i]<<endl;
+    }
+        
+        
+    }
+    return 0;
+    }
