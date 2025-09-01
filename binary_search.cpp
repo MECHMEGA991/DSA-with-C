0784695c9795119d83e67a420bfafed24e19c3b1
MECHMEGA991 <ankushbhandari664@gmail.com>
2025-09-01 18:58:29 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
+#define SIZE 7
 
+using namespace std;
+
+// binary search
+
+void binary(int arr[SIZE],int n){
+    
+    
+    int lower =0;
+    int higher = SIZE-1;
+    
+    
+    while(lower<=higher){
+        
+    int mid=(lower+higher)/2;
+    
+        if(arr[mid]==n){
+            cout<<"Element Found at mid "<<mid;
+            break;
+        }
+        else if(n<arr[mid]){
+            
+            higher=mid-1;
+            
+        }
+        else{
+            lower=mid+1;
+        }
+        
+    }
+  
+  
+    
+}
+
+
+int main(){
+    int arr[SIZE]={12,20,34,56,86,98,100};
+    int n;
+    cout<<"Enter the element u want too search=";
+    cin>>n;
+    
+    binary(arr,n);
+    
+    
+    return 0;
+}
